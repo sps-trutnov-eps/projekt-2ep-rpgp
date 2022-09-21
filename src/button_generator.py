c9b67dc28a82d25f9f427c4263b32c7b4e9cb19e
Vojtěch Nepimach <nepimachv21@spstrutnov.cz>
2022-09-21 22:54:35 +0200

    Experimentální kód
    Experimentální prototyp nástroje, který by mohl ulehčit tvoření klikatelných objektů.

---
+++ src/button_generator.py
@@ -0,0 +1,27 @@
+import sys
+import pygame as pg
+
+def area_select(surface):
+    area_selection = True
+    point_1 = None
+    point_2 = None
+    
+    while area_selection == True and point_1 == None and point_2 == None:
+        if point_1 == None and pg.mouse.get_pressed()[0]:
+            point_1 = round(pg.mouse.get_pos()[0]), round(pg.mouse.get_pos()[1]) 
+            
+        if point_2 == None and pg.mouse.get_pressed()[1]:
+            point_2 = round(pg.mouse.get_pos()[0]), round(pg.mouse.get_pos()[1]) 
+        
+        events = pg.event.get()
+        pressed = pg.key.get_pressed()
+        
+        for event in events:
+            if pressed[pg.K_o]:
+                area_selection = False
+        
+        if not point_1 == None:
+            if not point_2 == None:
+                pg.draw.rect(surface, (0,255,0), point_1, point_2)
+        
+    return point_1, point_2
--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 # --- Projekt Role-playing parodie Vojtěcha Nepimacha a Pavla Kotka ---
 import sys
 import pygame as pg
-from Screens import main_menu
+from Screens import *
+from button_generator import *
 
 if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
     DATA_ROOT = '.'
@@ -19,10 +20,15 @@ active_screen = main_menu
 
 while True:
     events = pg.event.get()
+    pressed = pg.key.get_pressed()
     screen.blit(active_screen.background,(0,0))
     for event in events:
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
+    
+        if pressed[pg.K_p]:
+            new_button_coordinates = area_select(screen)
+            print(new_button_coordinates)
     
     pg.display.update()
