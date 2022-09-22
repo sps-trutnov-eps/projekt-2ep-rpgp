a2c12d207f374e7dd23bf4b34b386fc7c5e12ebd
Pavel Kotek <kotekp21@spstrutnov.cz>
2022-09-22 16:39:52 +0200

    Nástroj tlačítkový byl stvořen

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 # --- Projekt Role-playing parodie Vojtěcha Nepimacha a Pavla Kotka ---
 import sys
 import pygame as pg
-from Screens import main_menu
+from screens import *
+from button_tool import *
 
 if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
     DATA_ROOT = '.'
@@ -24,5 +25,22 @@ while True:
         if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
+    
+    # Práce Button_tool
+    if pg.mouse.get_pressed()[0]:
+        bt.find_l()
+    if pg.mouse.get_pressed()[2]:
+        bt.find_r()
+        
+    corner1, corner2, width, height = bt.calculate()
+    
+    # Vypisování výsledků Button_tool
+    if pg.key.get_pressed()[pg.K_SPACE]:
+        print("Šířka: ", width)
+        print("Výška: ", height)
+        print("1. Roh: ", corner1[0], ", ", corner1[1])
+        print("2. Roh: ", corner2[0], ", ", corner2[1])
+    if not width == 0 and not height == 0:
+        pg.draw.rect(screen, (0,255,0), (corner1[0], corner1[1], width, height), 2)
     
     pg.display.update()
