91936fb71b80023c5d15b5a9f3670dc626004caa
Vojtěch Nepimach <nepimachv21@spstrutnov.cz>
2022-09-22 07:55:36 +0200

    Postup na Experimentálním nástroji.

--- src/button_generator.py
+++ src/button_generator.py
@@ -6,22 +6,23 @@ def area_select(surface):
     point_1 = None
     point_2 = None
     
-    while area_selection == True and point_1 == None and point_2 == None:
+    #while area_selection == True:
+    while point_1 == None or point_2 == None:
         if point_1 == None and pg.mouse.get_pressed()[0]:
             point_1 = round(pg.mouse.get_pos()[0]), round(pg.mouse.get_pos()[1]) 
             
-        if point_2 == None and pg.mouse.get_pressed()[1]:
+        if point_2 == None and pg.mouse.get_pressed()[2]:
             point_2 = round(pg.mouse.get_pos()[0]), round(pg.mouse.get_pos()[1]) 
         
         events = pg.event.get()
         pressed = pg.key.get_pressed()
         
-        for event in events:
-            if pressed[pg.K_o]:
-                area_selection = False
+        #for event in events:
+        #    if pressed[pg.K_o]:
+        #        area_selection = False
         
-        if not point_1 == None:
-            if not point_2 == None:
-                pg.draw.rect(surface, (0,255,0), point_1, point_2)
+        #if not point_1 == None:
+         #   if not point_2 == None:
+          #      pg.draw.rect(surface, (0,255,0), point_1, point_2)
         
     return point_1, point_2
--- src/main.py
+++ src/main.py
@@ -18,6 +18,9 @@ pg.display.set_caption("Generic Game")
 
 active_screen = main_menu
 
+new_button_coordinates = None
+final_coords = 0, 0, 0, 0
+
 while True:
     events = pg.event.get()
     pressed = pg.key.get_pressed()
@@ -29,6 +32,17 @@ while True:
     
         if pressed[pg.K_p]:
             new_button_coordinates = area_select(screen)
+            tuple1 = new_button_coordinates[0]
+            tuple2 = new_button_coordinates[1]
+            coord1 = int(tuple1[0])
+            coord2 = int(tuple1[1])
+            coord3 = int(tuple2[0])
+            coord4 = int(tuple2[1])
+            final_coords = coord1, coord2, coord3, coord4
             print(new_button_coordinates)
+            print(final_coords)
+            
+    pg.draw.rect(screen, (0,255,0), final_coords)
+    
     
     pg.display.update()
