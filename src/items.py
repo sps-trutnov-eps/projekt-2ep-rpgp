4597b05e87bdf41dcf8283ef8de2763c736bb16c
Vojtěch Nepimach <nepimachv21@spstrutnov.cz>
2022-10-26 22:27:44 +0200

    Připrava obchodu na další role.

--- src/items.py
+++ src/items.py
@@ -52,6 +52,84 @@ def init_items(weapons, role):
         weapon_desc_3_type_3 = "A spiked mace on a stick."
         weapon_desc_4_type_3 = "A spiky metal ball\non a chain."
         weapon_desc_5_type_3 = "The mythical hammer\nof the norse god\nof thunder, Thor."
+        
+    if role == "mage":
+        weapon_name_1_type_1 = "Wooden Sword"
+        weapon_name_2_type_1 = "Copper Sword"
+        weapon_name_3_type_1 = "Iron Dagger"
+        weapon_name_4_type_1 = "Fencing Sword"
+        weapon_name_5_type_1 = "Yankov Avenger"
+        
+        weapon_desc_1_type_1 = "A slight upgrade to your stick.\nStill not much to look at but\natleast its sword shaped."
+        weapon_desc_2_type_1 = "A crooked poorly crafted\ncopper sword."
+        weapon_desc_3_type_1 = "A small iron dagger. It doesn't\nhave much range but it won't break."
+        weapon_desc_4_type_1 = "A long, quality fencing sword.\nMuch better than anything\n you've hanled before."
+        weapon_desc_5_type_1 = "The Legendary sword of the demon\nslayer Ivanovic Yankov. It is imbued\nwith the essence of fire and originality."
+        
+        
+        weapon_name_1_type_2 = "Primitive Axe"
+        weapon_name_2_type_2 = "Copper Hatchet"
+        weapon_name_3_type_2 = "Lumberjack Axe"
+        weapon_name_4_type_2 = "Steel Waraxe"
+        weapon_name_5_type_2 = "Vorneag's Waraxe"
+        
+        weapon_desc_1_type_2 = "A sharpened stone\ntied to a branch."
+        weapon_desc_2_type_2 = "A chipped copper hatchet.\nIt has seen better days\nbut it still holds up."
+        weapon_desc_3_type_2 = "An axe of a lumberjack\n by profession."
+        weapon_desc_4_type_2 = "A large, hefty but\nproper waraxe."
+        weapon_desc_5_type_2 = "The Waraxe of the\nlegendary dwarf warrior\nVorneag Grimbow."
+        
+        
+        weapon_name_1_type_3 = "Wooden Mallet"
+        weapon_name_2_type_3 = "Steel Hammer"
+        weapon_name_3_type_3 = "Spiked Mace"
+        weapon_name_4_type_3 = "Metal Flail"
+        weapon_name_5_type_3 = "Thor's Mjölnir"
+        
+        weapon_desc_1_type_3 = "A wooden simple wooden\nmallet made from a stick\nand a piece of wood."
+        weapon_desc_2_type_3 = "A solid hammer made\nof cast steel."
+        weapon_desc_3_type_3 = "A spiked mace on a stick."
+        weapon_desc_4_type_3 = "A spiky metal ball\non a chain."
+        weapon_desc_5_type_3 = "The mythical hammer\nof the norse god\nof thunder, Thor."
+        
+    if role == "ranger":
+        weapon_name_1_type_1 = "Wooden Sword"
+        weapon_name_2_type_1 = "Copper Sword"
+        weapon_name_3_type_1 = "Iron Dagger"
+        weapon_name_4_type_1 = "Fencing Sword"
+        weapon_name_5_type_1 = "Yankov Avenger"
+        
+        weapon_desc_1_type_1 = "A slight upgrade to your stick.\nStill not much to look at but\natleast its sword shaped."
+        weapon_desc_2_type_1 = "A crooked poorly crafted\ncopper sword."
+        weapon_desc_3_type_1 = "A small iron dagger. It doesn't\nhave much range but it won't break."
+        weapon_desc_4_type_1 = "A long, quality fencing sword.\nMuch better than anything\n you've hanled before."
+        weapon_desc_5_type_1 = "The Legendary sword of the demon\nslayer Ivanovic Yankov. It is imbued\nwith the essence of fire and originality."
+        
+        
+        weapon_name_1_type_2 = "Primitive Axe"
+        weapon_name_2_type_2 = "Copper Hatchet"
+        weapon_name_3_type_2 = "Lumberjack Axe"
+        weapon_name_4_type_2 = "Steel Waraxe"
+        weapon_name_5_type_2 = "Vorneag's Waraxe"
+        
+        weapon_desc_1_type_2 = "A sharpened stone\ntied to a branch."
+        weapon_desc_2_type_2 = "A chipped copper hatchet.\nIt has seen better days\nbut it still holds up."
+        weapon_desc_3_type_2 = "An axe of a lumberjack\n by profession."
+        weapon_desc_4_type_2 = "A large, hefty but\nproper waraxe."
+        weapon_desc_5_type_2 = "The Waraxe of the\nlegendary dwarf warrior\nVorneag Grimbow."
+        
+        
+        weapon_name_1_type_3 = "Wooden Mallet"
+        weapon_name_2_type_3 = "Steel Hammer"
+        weapon_name_3_type_3 = "Spiked Mace"
+        weapon_name_4_type_3 = "Metal Flail"
+        weapon_name_5_type_3 = "Thor's Mjölnir"
+        
+        weapon_desc_1_type_3 = "A wooden simple wooden\nmallet made from a stick\nand a piece of wood."
+        weapon_desc_2_type_3 = "A solid hammer made\nof cast steel."
+        weapon_desc_3_type_3 = "A spiked mace on a stick."
+        weapon_desc_4_type_3 = "A spiky metal ball\non a chain."
+        weapon_desc_5_type_3 = "The mythical hammer\nof the norse god\nof thunder, Thor."
 
     starter_weapon = item("Stick", "Nothing but a very\ngeneric stick.", pg.image.load(DATA_ROOT + "/data/textures/weapons/stick.png"), ((resolution[0]/4) - 96, (resolution[1]/2) - 225), "Weapon board")
     starter_weapon.shown = True
