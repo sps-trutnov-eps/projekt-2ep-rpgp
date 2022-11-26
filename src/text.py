eb93041c22a38bd0d2e0f3bb36ad09fa68c2de9f
Pavel Kotek <kotekp21@spstrutnov.cz>
2022-11-26 13:26:11 +0100

    Z úmrtní tabulky se již dá dostat

--- src/text.py
+++ src/text.py
@@ -123,6 +123,9 @@ p_level = text(["Game menu", "Shop", "Campaign", "Profile", "Weapon board", "Arm
 pause_question = text(["Pause table"], "Do you wish to leave the battle?", (600,330), pg.font.Font(def_link, settings_size), def_colour)
 pause_yes = text(["Pause table"], "Yes", (450,542), pg.font.Font(def_link, 38), def_colour)
 pause_no = text(["Pause table"], "No", (750,542), pg.font.Font(def_link, 38), def_colour)
+death_statement = text(["Death table"], "You have lost the battle", (600,330), pg.font.Font(def_link, settings_size), def_colour)
+death_leave = text(["Death table"], "Leave", (450,542), pg.font.Font(def_link, 38), def_colour)
+death_retry = text(["Death table"], "Retry", (750,542), pg.font.Font(def_link, 38), def_colour)
 
 # Zprávy v ochodě
 buy = message("buy", ["Weapon board", "Armor board", "Item board"], "Item has been purchased", (300,150), pg.font.Font(def_link, message_size), dark_colour)
