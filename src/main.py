c2bb8242f466e27e64ec5a2a963216761c35e1fd
Pavel Kotek <kotekp21@spstrutnov.cz>
2022-11-30 21:04:37 +0100

    Ikonky statů v obchodě použity

--- src/main.py
+++ src/main.py
@@ -78,6 +78,9 @@ def work_buttons_and_texts():
     for t in text_class.all:
         t.blit_self(screen, on__screen)
         
+    for b in on__screen.bought_icons:
+        b.blit_self(screen, on__screen)
+        
 def work_devmode(devmode, dev_shortcut, bt_acc, tt_acc, pt_acc):
     ### DEVELOPER MODE ###
     if pressed[pg.K_d]:
